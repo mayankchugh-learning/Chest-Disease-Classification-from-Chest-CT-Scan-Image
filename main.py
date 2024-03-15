28c5e2b58c571ce4bd7ba22a107e30bc1497a377
mayankchugh1977 <mayank.chugh@gmail.com>
2024-03-15 11:01:07 +0800

    Stage 3 model trainer code component code

--- main.py
+++ main.py
@@ -31,3 +31,17 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+
+
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
